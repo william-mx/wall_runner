7554b80ec75d34ca2703fcc954854170f3dbfda9
wengel-hm <william.engel@mdynamix.de>
2025-11-09 22:40:48 +0100

    add launch and config dir

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'wall_runner'
 
@@ -7,9 +9,10 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*_launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
